aaca89e394343b1e616def0bbaebf49bdab81ee1
IbaaIbrahim <eng.ibaa.ibr@gmail.com>
2025-04-03 08:47:50 +0300

    cms add responsive

--- src/components/modules/common/cms/cms-content-items/cms-item-text-with-image.tsx
+++ src/components/modules/common/cms/cms-content-items/cms-item-text-with-image.tsx
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types';
 import React, { FC } from 'react'
 import DOMPurify from 'dompurify';
+import clsx from 'clsx'
 
 import styles from './cms-item-text-with-image.module.scss'
+import './cms-item-text-with-image.module.scss'
 import Image from 'next/image'
 
 const CmsItemEditor: FC<{contentItemData: any}> = ({ contentItemData }) => {
@@ -12,9 +14,9 @@ const CmsItemEditor: FC<{contentItemData: any}> = ({ contentItemData }) => {
 			<span style={{fontSize: 32, fontWeight: 500, marginBottom: '1rem'}} className={styles.title}>
 				{contentItemData?.data?.title}
 			</span>
-			<div className={styles.content}>
+			<div className={clsx(styles.content, 'cms-item-text-with-image-content-wrapper')} >
 				<div style={{paddingRight: 30}} dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(contentItemData?.data?.text) }} />
-				<Image style={{borderRadius: 10}} className={styles.image} width={200} height={200} src={contentItemData?.data?.file?.url} alt={contentItemData?.data?.file?.alt} />
+				<Image style={{borderRadius: 10}} className={clsx(styles.image, 'cms-item-text-with-image-content-image')} width={200} height={200} src={contentItemData?.data?.file?.url} alt={contentItemData?.data?.file?.alt} />
 			</div>
 		</div>
 	)
--- src/components/modules/common/cms/cms-content-items/cms-item-two-images.tsx
+++ src/components/modules/common/cms/cms-content-items/cms-item-two-images.tsx
@@ -4,18 +4,23 @@ import _ from 'lodash'
 
 import styles from './cms-item-two-images.module.scss'
 import Image from 'next/image'
+import clsx from 'clsx'
 
 const CmsItemTwoImages: FC<{contentItemData: any}> = ({ contentItemData }) => {
 
 	return (
 		<div className={styles.wrapper}>
-			<div className={styles.content}>
-				<div className={styles.imageItemWrapper}>
-					<Image style={{width: '100%', height: 230}} width={300} height={300} src={_.get(contentItemData, 'data.file1.url')} alt='' />
+			<div className={clsx(styles.content, 'cms-item-two-images-content')}>
+				<div className={clsx(styles.imageItemWrapper, 'cms-item-two-images-image-wrapper')}>
+					<div style={{position: 'relative', flex: '1 1 0'}}>
+						<Image fill src={_.get(contentItemData, 'data.file1.url')} alt='' />
+					</div>
 					<span style={{color: '#898989'}}>{_.get(contentItemData, 'data.text1')}</span>
 				</div>
-				<div className={styles.imageItemWrapper}>
-					<Image style={{width: '100%', height: 230}} width={300} height={300} src={_.get(contentItemData, 'data.file2.url')} alt='' />
+				<div className={clsx(styles.imageItemWrapper, 'cms-item-two-images-image-wrapper')}>
+					<div style={{position: 'relative', flex: '1 1 0'}}>
+						<Image fill src={_.get(contentItemData, 'data.file2.url')} alt='' />
+					</div>
 					<span style={{color: '#898989'}}>{_.get(contentItemData, 'data.text2')}</span>
 				</div>
 			</div>
